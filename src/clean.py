20fb3d9f8ec4d8f292da7ec521b2992842542d37
Ali Hejazizo <hejazizo@ualberta.ca>
2021-11-28 10:07:02 -0700

    update OrganizeFiles to get directory in obj call

--- src/clean.py
+++ src/clean.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+from typing import Union
 
 from loguru import logger
 
@@ -12,24 +13,24 @@ class OraganizeFiles:
     This class is used to organize files in a directory by
     moving files into directories based on extension.
     """
-    def __init__(self, directory):
-        self.directory = Path(directory)
-        if not self.directory.exists():
-            raise FileNotFoundError(f"{self.directory} does not exist")
-
+    def __init__(self):
         ext_dirs = read_json(DATA_DIR / "extensions.json")
         self.extensions_dest = {}
         for dir_name, ext_list in ext_dirs.items():
             for ext in ext_list:
                 self.extensions_dest[ext] = dir_name
 
-    def __call__(self):
+    def __call__(self, directory: Union[str, Path]):
         """ Organize files in a directory by moving them
         to sub directories based on extension.
         """
-        logger.info(f"Organizing files in {self.directory}...")
+        directory = Path(directory)
+        if not directory.exists():
+            raise FileNotFoundError(f"{directory} does not exist")
+
+        logger.info(f"Organizing files in {directory}...")
         file_extensions = []
-        for file_path in self.directory.iterdir():
+        for file_path in directory.iterdir():
             # ignore directories
             if file_path.is_dir():
                 continue
@@ -41,16 +42,15 @@ class OraganizeFiles:
             # move files
             file_extensions.append(file_path.suffix)
             if file_path.suffix not in self.extensions_dest:
-                DEST_DIR = self.directory / 'other'
+                DEST_DIR = directory / 'other'
             else:
-                DEST_DIR = self.directory / self.extensions_dest[file_path.suffix]
+                DEST_DIR = directory / self.extensions_dest[file_path.suffix]
 
             DEST_DIR.mkdir(exist_ok=True)
             logger.info(f"Moving {file_path} to {DEST_DIR}...")
             shutil.move(str(file_path), str(DEST_DIR))
 
 if __name__ == "__main__":
-    org_files = OraganizeFiles('/Users/ali/Downloads')
-    org_files()
+    org_files = OraganizeFiles()
+    org_files('/Users/ali/Downloads')
     logger.info("Done!")
-
