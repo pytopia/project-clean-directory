05bc41746cd725fae6e684edd5b4b60636ea88f1
Erfan Pro97 <ghaziasgar.seyederfan@gmail.com>
2023-08-09 13:10:06 +0330

    Add write to json function in io

--- src/utils/io.py
+++ src/utils/io.py
@@ -3,3 +3,8 @@ import json
 def read_json(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
+
+def write_json(file_path, data):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4, sort_keys=True)
+
